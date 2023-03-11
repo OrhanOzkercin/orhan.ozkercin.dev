9ded2a06683344ebf9d278ac645baab82add4355
Orhan Özkerçin <orhanozkercin@yahoo.com>
2023-03-11 14:06:18 +0300

    ga location update

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,26 @@
 import { DefaultLayout } from "@/layouts/default-layout";
 import "@/styles/globals.css";
 import { ThemeProvider } from "next-themes";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
   return (
-    <ThemeProvider attribute="class" enableSystem={false} defaultTheme="dark">
-      <DefaultLayout>
-        <Component {...pageProps} />
-      </DefaultLayout>
-    </ThemeProvider>
+    <>
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-3XWJXBWTG7" strategy="afterInteractive" />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-3XWJXBWTG7');
+        `}
+      </Script>
+      <ThemeProvider attribute="class" enableSystem={false} defaultTheme="dark">
+        <DefaultLayout>
+          <Component {...pageProps} />
+        </DefaultLayout>
+      </ThemeProvider>
+    </>
   );
 }
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,17 +1,9 @@
-import { Html, Head, Main, NextScript } from "next/document";
-import Script from "next/script";
+import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-3XWJXBWTG7" />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-3XWJXBWTG7');`}
-      </Script>
       <body>
         <Main />
         <NextScript />

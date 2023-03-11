efcd2b6ff97413d0f8ed3fe64c505575c156112d
Orhan Özkerçin <orhanozkercin@yahoo.com>
2023-03-11 14:12:41 +0300

    ga

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,20 +2,12 @@ import { DefaultLayout } from "@/layouts/default-layout";
 import "@/styles/globals.css";
 import { ThemeProvider } from "next-themes";
 import Script from "next/script";
+import { GoogleAnalytics } from "nextjs-google-analytics";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <Script src="https://www.googletagmanager.com/gtag/js?id=G-3XWJXBWTG7" strategy="afterInteractive" />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){window.dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'G-3XWJXBWTG7');
-        `}
-      </Script>
+      <GoogleAnalytics strategy="lazyOnload" trackPageViews />
       <ThemeProvider attribute="class" enableSystem={false} defaultTheme="dark">
         <DefaultLayout>
           <Component {...pageProps} />

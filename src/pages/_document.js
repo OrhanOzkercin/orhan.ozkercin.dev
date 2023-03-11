ca16e01f1d9260944810b8a808646ba085e1469c
Orhan Özkerçin <orhanozkercin@yahoo.com>
2023-03-11 13:25:05 +0300

    add google analytics

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,21 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <Script strategy="lazyOnload" src="https://www.googletagmanager.com/gtag/js?id=G-3XWJXBWTG7" />
+      <Script id="google-analytics" strategy="lazyOnload">
+        {`window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-3XWJXBWTG7');`}
+      </Script>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }

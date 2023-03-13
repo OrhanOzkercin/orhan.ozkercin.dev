84a5295a96b7ba3558d26a21d0da6155524989a7
Orhan Özkerçin <orhanozkercin@yahoo.com>
2023-03-13 10:33:47 +0300

    change social bg

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -14,7 +14,7 @@ export default function Document() {
           name="twitter:description"
           content="With over 3 years of experience in frontend development, I am a skilled developer specializing in JavaScript. My passion for building visually stunning and functional user interfaces has driven me to become proficient in several popular frameworks, including Angular, VueJS, and ReactJS."
         />
-        <meta name="twitter:image" content="social-bg.png" />
+        <meta name="twitter:image" content="https://orhan.ozkercin.dev/social-bg.png" />
         <meta property="og:url" content="http://orhan.ozkercin.dev" />
         <meta property="og:title" content="Orhan Özkerçin" />
         <meta

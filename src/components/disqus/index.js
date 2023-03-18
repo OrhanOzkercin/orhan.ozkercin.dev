9a69721f2df5dcb8883c2fa20d93629eba81d4ef
Orhan Özkerçin <orhanozkercin@yahoo.com>
2023-03-18 22:57:22 +0300

    change disqus settings

--- src/components/disqus/index.js
+++ src/components/disqus/index.js
@@ -1,16 +1,11 @@
 import { DiscussionEmbed } from "disqus-react";
 const DisqusComments = ({ post }) => {
   console.log("DisqusComments -> post:", post);
-  const disqusShortname = "@disqus_ODXgyxAwBT";
-  const disqusConfig = {
-    url: post.slug,
-    identifier: post.slug, // Single post id
-    title: post.title, // Single post title
-  };
+
   return (
     <div>
       <DiscussionEmbed
-        shortname="orhanozkercin"
+        shortname="orhan-ozkercin"
         config={{
           url: post.slug,
           identifier: post.slug, // Single post id
